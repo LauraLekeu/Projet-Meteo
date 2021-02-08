06fbec9063a1f1a25db92c1483d4b9082f10b02a
laura <lauralekeu13@gmail.com>
2021-02-08 23:41:09 +0100

    Géolocalisation

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
-// 30280ea9e4883e1f258f7a156be504a9 = API KEY
 
-let ville = "Aubel";
-recevoirTemperature(ville);
-// Appeler la fonction recevoirTemperature(ville); 
+let ville;
+
+if('geolocation' in navigator) {
+  navigator.geolocation.watchPosition((position) => {
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=30280ea9e4883e1f258f7a156be504a9&units=metric';
+    // console.log(url);
+
+    let requete = new XMLHttpRequest();
+
+    requete.open('GET', url);
+
+    requete.responseType = 'json';
+
+    requete.send(); // Envoyer la requete
+
+
+    // Détecter que l'API renvoit une réponse
+    requete.onload = function() {
+      if (requete.readyState === XMLHttpRequest.DONE) { // Si la requête est bien terminée
+        if (requete.status === 200 )  { // si la requête a été traité avec succès
+          let reponse = requete.response; // Récupérer la réponse de la requête
+          let temperature = reponse.main.temp; // Récupérer la donnée temp de la propriété main de la réponse de l'api
+          let ville = reponse.name;
+          document.querySelector('#temperature_label').textContent = temperature;
+          document.querySelector('#ville').textContent = ville;
+        }
+        else {
+          alert('Problème ');
+        }
+      }
+    }
+  }, erreur, options);
+}
+else {
+  ville = "Paris";
+  recevoirTemperature(ville);
+}
+
+var options = {
+
+  enableHighAccuracy: true
+}
 
 let  btnChangerVille = document.querySelector('#changer');
 
 btnChangerVille.addEventListener('click', () => {
-  ville = prompt("Choix de la ville :"); 
+  ville = prompt("Choix de la ville :");
   recevoirTemperature(ville)
 });
+
+function erreur() {
+  ville = "Paris";
+  recevoirTemperature(ville);
+}
 
 
 function recevoirTemperature(ville) {
@@ -24,7 +68,7 @@ function recevoirTemperature(ville) {
 
   requete.send(); // Envoyer la requete
 
- 
+
   // Détecter que l'API renvoit une réponse
   requete.onload = function() {
     if (requete.readyState === XMLHttpRequest.DONE) { // Si la requête est bien terminée
@@ -41,4 +85,3 @@ function recevoirTemperature(ville) {
     }
   }
 }
- 
